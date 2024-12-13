93de6d71b4b2762d8683b01e971455e6a23459bd
silvi-ss <105846692+silvi-ss@users.noreply.github.com>
2024-12-13 22:33:29 +0200

    Initial commit: Added HomePro.Web project

---
+++ HomePro/HomePro.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomePro.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
