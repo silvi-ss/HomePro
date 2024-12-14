442aff601fad82fb214592977737e7b78dd9d88e
silvi-ss <105846692+silvi-ss@users.noreply.github.com>
2024-12-14 12:50:24 +0200

    Add HomePro.Data project and move ApplicationDbContext with Identity migrations

--- HomePro/HomePro.Data/ApplicationDbContext.cs
+++ HomePro/HomePro.Data/ApplicationDbContext.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace HomePro.Web.Data
+namespace HomePro.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
--- HomePro/HomePro.Web/Program.cs
+++ HomePro/HomePro.Web/Program.cs
@@ -1,4 +1,4 @@
-using HomePro.Web.Data;
+using HomePro.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
